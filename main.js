bbc47de12b68c513ff1611c7e3aa4ee1d08b93c0
Arbenitamusaj <95128490+Arbenitamusaj@users.noreply.github.com>
2021-12-29 00:04:12 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,139 @@
+var btn1= document.getElementById('btn1')
+var audio1=new Audio("mp3/a1.mp3");
+btn1.onclick= function()
+{
+	btn1.innerHTML="Angelina Aludo";
+	audio1.play();
+
+};
+
+var btn2= document.getElementById('btn2')
+var audio2=new Audio("mp3/a2.mp3");
+btn2.onclick= function()
+{
+	btn2.innerHTML="Harper Lee";
+	audio2.play();
+};
+
+
+var btn3= document.getElementById('btn3')
+var audio3=new Audio("mp3/a3.mp3");
+btn3.onclick= function()
+{
+	btn3.innerHTML="Suzane Collins";
+	audio3.play();
+};
+var btn4= document.getElementById('btn4')
+var audio4=new Audio("mp3/a4.mp3");
+btn4.onclick= function()
+{
+	btn4.innerHTML="Markus Zusak";
+	audio4.play();
+};
+var btn5= document.getElementById('btn5')
+var audio5=new Audio("mp3/a5.mp3");
+btn5.onclick= function()
+{
+	btn5.innerHTML="William Shakespeare";
+	audio5.play();
+};
+var btn6= document.getElementById('btn6')
+var audio6=new Audio("mp3/a6.mp3");
+btn6.onclick= function()
+{
+	btn6.innerHTML="J. K. Rowling";
+	audio6.play();
+};
+var btn7= document.getElementById('btn7')
+var audio7=new Audio("mp3/a7.mp3");
+btn7.onclick= function()
+{
+	btn7.innerHTML="Khaled Hosseini";
+	audio7.play();
+};
+var btn8= document.getElementById('btn8')
+var audio8=new Audio("mp3/a8.mp3");
+btn8.onclick= function()
+{
+	btn8.innerHTML="Tolkien";
+	audio8.play();
+};
+var btn9= document.getElementById('btn9')
+var audio9=new Audio("mp3/a9.mp3");
+btn9.onclick= function()
+{
+	btn9.innerHTML="George Orwell";
+	audio9.play();
+};
+var btn10= document.getElementById('btn10')
+var audio10=new Audio("mp3/a10.mp3");
+btn10.onclick= function()
+{
+	btn10.innerHTML="Emily Bronte";
+	audio10.play();
+};
+var btn11= document.getElementById('btn11')
+var audio11=new Audio("mp3/a11.mp3");
+btn11.onclick= function()
+{
+	btn11.innerHTML="Jane Austen";
+	audio11.play();
+};
+var btn12= document.getElementById('btn12')
+var audio12=new Audio("mp3/a12.mp3");
+btn12.onclick= function()
+{
+	btn12.innerHTML="Lois Lowry";
+	audio12.play();
+};
+var btn13= document.getElementById('btn13')
+var audio13=new Audio("mp3/a13.mp3");
+btn13.onclick= function()
+{
+	btn13.innerHTML="John Steinbeck";
+	audio13.play();
+};
+var btn14= document.getElementById('btn14')
+var audio14=new Audio("mp3/a14.mp3");
+btn14.onclick= function()
+{
+	btn14.innerHTML="Homer";
+	audio14.play();
+};
+var btn15= document.getElementById('btn15')
+var audio15=new Audio("mp3/a15.mp3");
+btn15.onclick= function()
+{
+	btn15.innerHTML="S.E.Hinton";
+	audio15.play();
+};
+var btn16= document.getElementById('btn16')
+var audio16=new Audio("mp3/a16.mp3");
+btn16.onclick= function()
+{
+	btn16.innerHTML="Charles Dickens";
+	audio16.play();
+};
+var btn17= document.getElementById('btn17')
+var audio17=new Audio("mp3/a17.mp3");
+btn17.onclick= function()
+{
+	btn17.innerHTML="Leon Tolstoi";
+	audio17.play();
+};
+
+var btn18= document.getElementById('btn18')
+var audio18=new Audio("mp3/a18.mp3");
+btn18.onclick= function()
+{
+	btn18.innerHTML="Ernest Hemingway";
+	audio18.play();
+};
+
+var btn19= document.getElementById('btn19')
+var audio19=new Audio("mp3/a19.mp3");
+btn19.onclick= function()
+{
+	btn19.innerHTML="Barbara Kingsolver";
+	audio19.play();
+};
